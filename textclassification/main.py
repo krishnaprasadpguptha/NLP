1ac0cbc509a014a2bae2b18675175c1e0840bd8f
krishnaprasd <krishnaprasad.poolakundil@landmark.co.uk>
2022-06-01 14:08:21 +0100

    update package list

--- textclassification/main.py
+++ textclassification/main.py
@@ -19,3 +19,10 @@ url = 'https://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz'
 dataset = tf.keras.utils.get_file('aclImdb_v1.tar.gz', url,
                                   untar=True, cache_dir='.',
                                   cache_subdir='')
+dataset_dir = os.path.join(os.path.dirname(dataset), 'aclImdb')
+
+train_dir = os.path.join(dataset_dir, 'train')
+
+# remove unused folders to make it easier to load the data
+remove_dir = os.path.join(train_dir, 'unsup')
+shutil.rmtree(remove_dir)
